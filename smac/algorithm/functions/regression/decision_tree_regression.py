4b42f8ec45e907c595aa29f98b9a3f0482b2b375
Javier Fernández-Bravo Peñuela <fcojavier.fernandezbravo.uclm@gmail.com>
2020-04-22 17:58:46 +0200

    Regression algorithm: Decision trees

--- smac/algorithm/functions/regression/decision_tree_regression.py
+++ smac/algorithm/functions/regression/decision_tree_regression.py
@@ -18,7 +18,7 @@ from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.ml.feature import VectorIndexer
 from pyspark.ml.evaluation import RegressionEvaluator
 
-from ..aux_functions import hyperparameters_values
+from ...aux_functions import hyperparameters_values
 
 hyperparameters_default_values = {
     'maxDepth': 5,
